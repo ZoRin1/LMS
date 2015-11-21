81be4cddb4e5a92f69539a780b458e4e40d745a7
ZoRin1 <842554169@qq.com>
2015-11-21 23:39:12 +0800

    a

--- LMSservice/src/rmiService/documents/addDocummentInfoDataSerImpl.java
+++ LMSservice/src/rmiService/documents/addDocummentInfoDataSerImpl.java
@@ -6,8 +6,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import po.documentsPO.DocumentPO;
+import po.documentsPO.OrderPO;
 import po.documentsPO.OutbillsPO;
 import po.documentsPO.PaymentPO;
 import dataservice.documentsdataservice.addDocummentInfoDataSer;
@@ -34,20 +36,45 @@ public class addDocummentInfoDataSerImpl extends UnicastRemoteObject implements 
 		try {
 			Class.forName(DRIVER);
 			Connection connection=DriverManager.getConnection(URL, USER, PASSWORD);
-			
+			PreparedStatement preparedStatement;
 			switch (po.getDoName()) {
 			case "出库单":
 				OutbillsPO outbillsPO=(OutbillsPO)po;
 				sql="insert into b出库单 (code,doName,packCode,date,account,arrival,Mode,transCode,examined) values ("+outbillsPO.getCode()+","+outbillsPO.getDoName()+","+outbillsPO.getPackCode()+","+outbillsPO.getDate()+","+outbillsPO.getAccount()+","+outbillsPO.getArrival()+","+outbillsPO.getMode()+","+outbillsPO.getTransCode()+",0)";
-				PreparedStatement preparedStatement=connection.prepareStatement(sql);
+				preparedStatement=connection.prepareStatement(sql);
 				preparedStatement.executeUpdate();
 				preparedStatement.close();
 				connection.close();
 				return true;
 			case "付款单":
 				PaymentPO paymentPO=(PaymentPO)po;
-				
+				sql="insert into b付款单(code,doName,date,account,fund,name,accoun2,type,remark,examined) values ("+paymentPO.getCode()+","+paymentPO.getDoName()+","+paymentPO.getDate()+","+paymentPO.getAccount()+","+paymentPO.getFund()+","+paymentPO.getName2()+","+paymentPO.getAccount2()+","+paymentPO.getType()+","+paymentPO.getRemark()+",0)";
+				preparedStatement=connection.prepareStatement(sql);
+				preparedStatement.executeUpdate();
+				preparedStatement.close();
+				connection.close();
+				return true;
 			case "寄件单":
+				OrderPO orderPO=(OrderPO)po;
+				String codeString=orderPO.getCode();
+				String doNameString=orderPO.getDoName();
+				String accountString=orderPO.getAccount();
+				String dateString=orderPO.getDate();
+				String senderNameString=orderPO.getSenderName();
+				String senderAddressString=orderPO.getSenderAddress();
+				String senderOrgString=orderPO.getSenderOrg();
+				String sPhoneNumberString=orderPO.getSPhoneNumber();
+				String sMobileNumberString=orderPO.getSMobileNumber();
+				String receiverNameString=orderPO.getReceiverName();
+				String receiverAddressString=orderPO.getReceiverAddress();
+				String receiverOrgString=orderPO.getReceiverOrg();
+				String rPhoneNumberString=orderPO.getRPhoneNumber();
+				String rMobileNumberString=orderPO.getRMobileNumber();
+				int number=orderPO.getNumber();
+				double weight=orderPO.getWeight();
+				double shape=orderPO.getShape();
+				
+				sql="insert into b寄件单(code,doName,account,date,SenderName,SenderAddress,SenderOrg,SPhoneNumber,SMobileNumber,ReceiverName,ReceiverAddress,ReceiverOrg,RPhoneNumber,RMobileNumber,number,weight,shape,cargoNamerList,sizeList,sumCost,state,examined)";
 			case "派件单":
 				break;
 			case "入库单":
