9f36cb0777208d9a8cdb5413f36383eaf255f20d
ZoRin1 <842554169@qq.com>
2015-11-22 00:14:49 +0800

    烦

--- LMSservice/src/rmiService/documents/getBufferedInfoDataSerImpl.java
+++ LMSservice/src/rmiService/documents/getBufferedInfoDataSerImpl.java
@@ -45,37 +45,37 @@ public class getBufferedInfoDataSerImpl extends UnicastRemoteObject implements g
 			resultSet.next();
 			switch (doName) {
 			case "出库单":
-				if (resultSet.getString(7).equals("PLANE")) {
-					return new OutbillsPO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6),ModeofTrans.PLANE, resultSet.getString(8));
-				}
-				else if (resultSet.getString(7).equals("TRAIN")) {
-					return new OutbillsPO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6),ModeofTrans.TRAIN, resultSet.getString(8));
-				}
-				else{
-					return new OutbillsPO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6),ModeofTrans.TRUCK, resultSet.getString(8));
-				}
+//				if (resultSet.getString(7).equals("PLANE")) {
+//					return new OutbillsPO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6),ModeofTrans.PLANE, resultSet.getString(8));
+//				}
+//				else if (resultSet.getString(7).equals("TRAIN")) {
+//					return new OutbillsPO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6),ModeofTrans.TRAIN, resultSet.getString(8));
+//				}
+//				else{
+//					return new OutbillsPO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6),ModeofTrans.TRUCK, resultSet.getString(8));
+//				}
 			case "付款单":
 				return new PaymentPO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getDouble(5), resultSet.getString(6), resultSet.getString(7), resultSet.getString(8),resultSet.getString(9));
 			case "寄件单":
-				ArrayList<String> arrayList=new ArrayList<String>();
-				String s[]=resultSet.getString(18).split(",");
-				for (int i = 0; i < s.length; i++) {
-					arrayList.add(s[i]);
-				}
-				double sizeList[]=new double[3];
-				String s2[]=resultSet.getString(19).split(",");
-				for (int i = 0; i < sizeList.length; i++) {
-					sizeList[i]=Double.parseDouble(s2[i]);
-				}
-				if (resultSet.getString(21).equals("QUICK")) {
-					return new OrderPO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6), resultSet.getString(7), resultSet.getString(8), resultSet.getString(9), resultSet.getString(10), resultSet.getString(11), resultSet.getString(12), resultSet.getString(13), resultSet.getString(14), resultSet.getInt(15), resultSet.getDouble(16), resultSet.getDouble(17), arrayList, sizeList, resultSet.getDouble(20),resultSet.getString(21));
-				}
-				else if (resultSet.getString(21).equals("SIMPLE")) {
-					return new OrderPO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6), resultSet.getString(7), resultSet.getString(8), resultSet.getString(9), resultSet.getString(10), resultSet.getString(11), resultSet.getString(12), resultSet.getString(13), resultSet.getString(14), resultSet.getInt(15), resultSet.getDouble(16), resultSet.getDouble(17), arrayList, sizeList, resultSet.getDouble(20), resultSet.getString(21));
-				}
-				else {
-					return new OrderPO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6), resultSet.getString(7), resultSet.getString(8), resultSet.getString(9), resultSet.getString(10), resultSet.getString(11), resultSet.getString(12), resultSet.getString(13), resultSet.getString(14), resultSet.getInt(15), resultSet.getDouble(16), resultSet.getDouble(17), arrayList, sizeList, resultSet.getDouble(20), resultSet.getString(21));
-				}
+//				ArrayList<String> arrayList=new ArrayList<String>();
+//				String s[]=resultSet.getString(18).split(",");
+//				for (int i = 0; i < s.length; i++) {
+//					arrayList.add(s[i]);
+//				}
+//				double sizeList[]=new double[3];
+//				String s2[]=resultSet.getString(19).split(",");
+//				for (int i = 0; i < sizeList.length; i++) {
+//					sizeList[i]=Double.parseDouble(s2[i]);
+//				}
+//				if (resultSet.getString(21).equals("QUICK")) {
+//					return new OrderPO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6), resultSet.getString(7), resultSet.getString(8), resultSet.getString(9), resultSet.getString(10), resultSet.getString(11), resultSet.getString(12), resultSet.getString(13), resultSet.getString(14), resultSet.getInt(15), resultSet.getDouble(16), resultSet.getDouble(17), arrayList, sizeList, resultSet.getDouble(20),resultSet.getString(21));
+//				}
+//				else if (resultSet.getString(21).equals("SIMPLE")) {
+//					return new OrderPO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6), resultSet.getString(7), resultSet.getString(8), resultSet.getString(9), resultSet.getString(10), resultSet.getString(11), resultSet.getString(12), resultSet.getString(13), resultSet.getString(14), resultSet.getInt(15), resultSet.getDouble(16), resultSet.getDouble(17), arrayList, sizeList, resultSet.getDouble(20), resultSet.getString(21));
+//				}
+//				else {
+//					return new OrderPO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6), resultSet.getString(7), resultSet.getString(8), resultSet.getString(9), resultSet.getString(10), resultSet.getString(11), resultSet.getString(12), resultSet.getString(13), resultSet.getString(14), resultSet.getInt(15), resultSet.getDouble(16), resultSet.getDouble(17), arrayList, sizeList, resultSet.getDouble(20), resultSet.getString(21));
+//				}
 			case "派件单":
 				break;
 			case "入库单":
