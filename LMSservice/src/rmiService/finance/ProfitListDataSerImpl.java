03a7b6cd1b9b4dd00ed5737bd4eb5b3a5607d79d
bufapiqi <26716594@qq.com>
2015-11-22 13:42:22 +0800

    成本收益记录实现

--- LMSservice/src/rmiService/finance/ProfitListDataSerImpl.java
+++ LMSservice/src/rmiService/finance/ProfitListDataSerImpl.java
@@ -2,8 +2,14 @@ package rmiService.finance;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
+import po.financePO.AccountPO;
 import po.financePO.ProfitPO;
 import dataservice.financedataservice.ProfitListDataSer;
 
@@ -26,7 +32,26 @@ public class ProfitListDataSerImpl extends UnicastRemoteObject implements Profit
 	@Override
 	public ArrayList<ProfitPO> ProList(String date)  throws RemoteException{
 		// TODO 自动生成的方法存根
-		return null;
+		
+		sql="SELECT * from 收益记录表";
+		ArrayList<ProfitPO> proPO = new ArrayList<ProfitPO>();
+		try {
+			Class.forName(DRIVER);
+			Connection connection=DriverManager.getConnection(URL, USER, PASSWORD);
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			ResultSet resultSet=preparedStatement.executeQuery();
+			while(resultSet.next()){
+				ProfitPO pro = new ProfitPO(resultSet.getDouble(1), resultSet.getDouble(2), resultSet.getString(3));
+				proPO.add(pro);
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return proPO;
 	}
 
 }
