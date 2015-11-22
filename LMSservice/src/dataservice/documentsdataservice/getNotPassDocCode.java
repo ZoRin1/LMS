b75d15ef60eccf188df650f4cbf4f79bf35bdd3d
ZoRin1 <842554169@qq.com>
2015-11-22 18:21:19 +0800

    修改下方法注释

--- LMSservice/src/dataservice/documentsdataservice/getNotPassDocCode.java
+++ LMSservice/src/dataservice/documentsdataservice/getNotPassDocCode.java
@@ -1,11 +1,12 @@
 package dataservice.documentsdataservice;
 
 import java.util.ArrayList;
-	/**
-	 * 通过创建人账号获得其所有未通过单据的编码和单据名
-	 * @author XiongKaiQi
-	 *
-	 */
+	
 	public interface getNotPassDocCode {
+		/**
+		 * 通过创建人账号获得其所有未通过单据的编码和单据名
+		 * @author XiongKaiQi
+		 *
+		 */
 		public ArrayList<String> getNotPassDocCode(String account);
 }
