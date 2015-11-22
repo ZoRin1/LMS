4e4035f33ae9cb9a42e784e530a3d468e290d576
DoctorBear <2948363603@qq.com>
2015-11-22 17:22:42 +0800

    注释修改

--- LMSservice/src/dataservice/documentsdataservice/getCodeDataSer.java
+++ LMSservice/src/dataservice/documentsdataservice/getCodeDataSer.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public interface getCodeDataSer {
 	/**
 	 * 
-	 * 系统返回一段时间内所有响应类型的单据的编码
+	 * 系统返回一段时间内所有响应类型的单据的编码和单据名
 	 * starttime可以为空，此时返回endtime之前所有单据
 	 * @author XiongKaiQi
 	 *
@@ -13,7 +13,7 @@ public interface getCodeDataSer {
 	public ArrayList<String> getCode(String doName,String startTime,String endTime);
 	/**
 	 * 
-	 * 系统返回一段时间内所有账号对应响应类型的单据的编码
+	 * 系统返回一段时间内所有账号对应响应类型的单据的编码和单据名
 	 * starttime可以为空，此时返回endtime之前所有单据
 	 * @author XiongKaiQi
 	 *
