15668c7cc45b918bd188149c1c4618eec90e22a8
ZoRin1 <842554169@qq.com>
2015-11-24 00:34:56 +0800

    完成transportationRMI

--- LMSservice/src/rmiService/transportation/TransportationDataSerImpl.java
+++ LMSservice/src/rmiService/transportation/TransportationDataSerImpl.java
@@ -2,6 +2,12 @@ package rmiService.transportation;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import po.transpotationPO.RoutePO;
 import dataservice.transportationdataservice.TransportationDataSer;
@@ -25,48 +31,180 @@ public class TransportationDataSerImpl extends UnicastRemoteObject implements Tr
 	@Override
 	public boolean updateDistance(RoutePO route) throws RemoteException{
 		// TODO 自动生成的方法存根
+		sql="update 线路 set distance='"+route.getDistance()+"' where line ='"+route.getLine()+"'";
+		try {
+			Class.forName(DRIVER);
+			Connection connection=DriverManager.getConnection(URL, USER, PASSWORD);
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			preparedStatement.executeUpdate();
+			connection.close();
+			return true;
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return false;
 	}
 
 	@Override
 	public boolean updateCost(int type, double cost)throws RemoteException {
 		// TODO 自动生成的方法存根
+		sql="update 成本常量 set cost='"+cost+"' where type ='"+type+"'";
+		try {
+			Class.forName(DRIVER);
+			Connection connection=DriverManager.getConnection(URL, USER, PASSWORD);
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			preparedStatement.executeUpdate();
+			connection.close();
+			return true;
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return false;
 	}
 
 	@Override
 	public double findCost(int type) throws RemoteException{
 		// TODO 自动生成的方法存根
+		sql="select cost from 成本常量 where type ='"+type+"'";
+		try {
+			Class.forName(DRIVER);
+			Connection connection=DriverManager.getConnection(URL, USER, PASSWORD);
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			ResultSet resultSet=preparedStatement.executeQuery();
+			resultSet.next();
+			double cost=resultSet.getDouble(1);
+			connection.close();
+			return cost;
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return 0;
 	}
 
 	@Override
 	public boolean insertRoute(RoutePO route) throws RemoteException{
 		// TODO 自动生成的方法存根
+		sql="insert into 线路 (line,distance) values('"+route.getLine()+"','"+route.getDistance()+"')";
+				try {
+					Class.forName(DRIVER);
+					Connection connection=DriverManager.getConnection(URL, USER, PASSWORD);
+					PreparedStatement preparedStatement=connection.prepareStatement(sql);
+					preparedStatement.executeUpdate();
+					connection.close();
+					return true;
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		return false;
 	}
 
 	@Override
 	public double findDistance(String line) throws RemoteException{
 		// TODO 自动生成的方法存根
+		sql="select distance from 线路 where line ='"+line+"'";
+		try {
+			Class.forName(DRIVER);
+			Connection connection=DriverManager.getConnection(URL, USER, PASSWORD);
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			ResultSet resultSet=preparedStatement.executeQuery();
+			resultSet.next();
+			double distance=resultSet.getDouble(1);
+			connection.close();
+			return distance;
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return 0;
 	}
 
 	@Override
 	public String[] findCityList(String city) throws RemoteException{
 		// TODO 自动生成的方法存根
+		sql="select city from 城市 where city <>'"+city+"'";
+		ArrayList<String>cityList=new ArrayList<String>();
+		try {
+			Class.forName(DRIVER);
+			Connection connection=DriverManager.getConnection(URL, USER, PASSWORD);
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			ResultSet resultSet=preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				cityList.add(resultSet.getString(1));
+			}
+			connection.close();
+			String [] citys=new String[cityList.size()];
+			for (int i = 0; i < citys.length; i++) {
+				citys[i]=cityList.get(i);
+			}
+			return citys;
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	@Override
 	public boolean insertCity(String city) throws RemoteException{
 		// TODO 自动生成的方法存根
+		sql="insert into 城市 (city) values('"+city+"')";
+		try {
+			Class.forName(DRIVER);
+			Connection connection=DriverManager.getConnection(URL, USER, PASSWORD);
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			preparedStatement.executeUpdate();
+			connection.close();
+			return true;
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return false;
 	}
 
 	@Override
 	public boolean removeCity(String city) throws RemoteException{
 		// TODO 自动生成的方法存根
+		sql="delete from 城市 where city='"+city+"'";
+		try {
+			Class.forName(DRIVER);
+			Connection connection=DriverManager.getConnection(URL, USER, PASSWORD);
+			PreparedStatement preparedStatement=connection.prepareStatement(sql);
+			preparedStatement.executeUpdate();
+			connection.close();
+			return true;
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return false;
 	}
 
