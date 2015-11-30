af0235874a80949b801d21f38f4971d2b5190d6b
bufapiqi <26716594@qq.com>
2015-11-30 19:35:32 +0800

    已经实现透明表格，有些东西可以直接复制来用

--- LMS/src/presentation/icwarehousemanui/checkDepotPanel.java
+++ LMS/src/presentation/icwarehousemanui/checkDepotPanel.java
@@ -1,10 +1,16 @@
 package presentation.icwarehousemanui;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
 
 public class checkDepotPanel extends JPanel{
 	
@@ -13,6 +19,7 @@ public class checkDepotPanel extends JPanel{
 	private JButton returnButton;
 	private JLabel j1;
 	private inDepotCheckJTable inDepotCheckJTable;
+//	private JTable inDepotTable;
 	
 	public checkDepotPanel(icwarehousemanui icwarehousemanui,icwarehousemanJpanel icwarehousemanJpanel){
 		init();
@@ -28,13 +35,24 @@ public class checkDepotPanel extends JPanel{
 		j1 = new JLabel(kuangjia);
 		j1.setBounds(0, 0, 720, 570);
 		
-//		inDepotCheckJTable = new inDepotCheckJTable(this);
+		inDepotCheckJTable = new inDepotCheckJTable(this);
+		
+//		initTable();
+//		JScrollPane scrollPane = new JScrollPane(inDepotTable); 
+//		scrollPane.getViewport().setOpaque(false);
+//		scrollPane.setOpaque(false);
+//		scrollPane.setViewportView(inDepotTable);
+//        scrollPane.setColumnHeaderView(inDepotTable.getTableHeader());
+//        
+//        scrollPane.getColumnHeader().setOpaque(false);
+//		scrollPane.setBounds(0, 57, 730, 475);
 		
 		returnButton.setBounds(662, 575,48,48);
 		returnButton.setContentAreaFilled(false);
 		
 		this.add(j1);
 		this.add(returnButton);
+		j1.add(inDepotCheckJTable.getScrollPane());
 		this.setOpaque(false);
 		this.setBounds(260, 60, 730,650);
 		this.setLayout(null);
@@ -56,4 +74,55 @@ public class checkDepotPanel extends JPanel{
 	    super.paintComponent(g);    
 	    g.drawImage(frameIcon.getImage(),-7,-12,null);
      }
+	
+//	private void initTable(){
+//		//假设的数据
+//		String[] inDepotName = new String[]{" "," "," "," "," "};
+//		
+//		Object[][] inDepotValue = new Object[][]{{"楚留","2321","fyk","2012-07-28 19:36:21","545"},
+//				{"楚留奇","232134 KB","fykhlp","2012-07-28 19:36:21","455"},
+//			{"楚留奇","2324 KB","fhlp","2012-07-28 19:36:21","454545"},
+//					{"楚香传奇","2324 KB","fykhlp","2012-07-28 19:36:21","455"},
+//				{"楚奇","232134 KB","fykhlp","2012-07-28 19:36:21","455"}
+//					};
+//		//假设的数据： 完善后要从数据库拿取数据来填写表格
+//		
+//		DefaultTableModel tableModel = new DefaultTableModel(inDepotValue,inDepotName);
+//		
+//		inDepotTable = new JTable(tableModel);
+//		inDepotTable.setRowHeight(32);
+//		inDepotTable.setShowGrid(false);
+//		TableColumn column = null;
+//		column = inDepotTable.getColumnModel().getColumn(0);
+//		column.setPreferredWidth(238);
+//		column = inDepotTable.getColumnModel().getColumn(1);
+//		column.setPreferredWidth(122);
+//		column = inDepotTable.getColumnModel().getColumn(2);
+//		column.setPreferredWidth(122);
+//		column = inDepotTable.getColumnModel().getColumn(3);
+//		column.setPreferredWidth(122);
+//		column = inDepotTable.getColumnModel().getColumn(4);
+//		column.setPreferredWidth(122);
+//		
+//		
+//		inDepotTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+//		inDepotTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); 
+//		
+//		inDepotTable.setOpaque(false); 
+//		DefaultTableCellRenderer render = new DefaultTableCellRenderer();   
+//		render.setOpaque(false);
+//		
+//		inDepotTable.setDefaultRenderer(Object.class,render);
+//		
+//		JTableHeader header = inDepotTable.getTableHeader();
+//		header.setOpaque(false);
+//		header.getTable().setOpaque(false);
+//		
+//		header.setDefaultRenderer(render); 
+//		TableCellRenderer headerRenderer = header.getDefaultRenderer();
+//		if (headerRenderer instanceof JLabel) {
+//			((JLabel) headerRenderer).setHorizontalAlignment(JLabel.CENTER);
+//			((JLabel) headerRenderer).setOpaque(false); 
+//		}
+//	}
 }
--- LMS/src/presentation/icwarehousemanui/inDepotCheckJTable.java
+++ LMS/src/presentation/icwarehousemanui/inDepotCheckJTable.java
@@ -4,67 +4,77 @@ import java.awt.*;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
 
 public class inDepotCheckJTable {
 	private JTable inDepotTable;
 	private checkDepotPanel checkDepotPanel;
+	private JScrollPane scrollPane;
 	
+	public JScrollPane getScrollPane() {
+		return scrollPane;
+	}
 	public inDepotCheckJTable(checkDepotPanel checkDepotPanel){
-		System.out.println("haha");
+		this.checkDepotPanel = checkDepotPanel;
 		initTable();
-		JScrollPane scrollPane = new JScrollPane(); 
+		init();
+	}
+	private void init(){
+        scrollPane = new JScrollPane(inDepotTable); 
 		scrollPane.getViewport().setOpaque(false);
 		scrollPane.setOpaque(false);
 		scrollPane.setViewportView(inDepotTable);
         scrollPane.setColumnHeaderView(inDepotTable.getTableHeader());
         
         scrollPane.getColumnHeader().setOpaque(false);
+		scrollPane.setBounds(0, 57, 730, 475);
         
         checkDepotPanel.add(scrollPane);
-//		init();
 	}
-//	private void init(){
-//		JScrollPane scrollPane = new JScrollPane(); 
-//		scrollPane.getViewport().setOpaque(false);
-//		scrollPane.setOpaque(false);
-//		scrollPane.setViewportView(inDepotTable);
-//        scrollPane.setColumnHeaderView(inDepotTable.getTableHeader());
-//        
-//        scrollPane.getColumnHeader().setOpaque(false);
-//        
-//        checkDepotPanel.add(scrollPane);
-//	}
 	
 	private void initTable(){
+		//假设的数据
 		String[] inDepotName = new String[]{" "," "," "," "," "};
 		
-		Object[][] inDepotValue = new Object[][]{{"楚留香传奇","232134 KB","fykhlp","2012-07-28 19:36:21","454545"},
-				{"楚留香传奇","232134 KB","fykhlp","2012-07-28 19:36:21","454545"},
-			{"楚留香传奇","232134 KB","fykhlp","2012-07-28 19:36:21","454545"},
-					{"楚留香传奇","232134 KB","fykhlp","2012-07-28 19:36:21","454545"},
-				{"楚留香传奇","232134 KB","fykhlp","2012-07-28 19:36:21","454545"}
+		Object[][] inDepotValue = new Object[][]{{"楚留","2321","fyk","2012-07-28 19:36:21","545"},
+				{"楚留奇","232134 KB","fykhlp","2012-07-28 19:36:21","455"},
+			{"楚留奇","2324 KB","fhlp","2012-07-28 19:36:21","454545"},
+					{"楚香传奇","2324 KB","fykhlp","2012-07-28 19:36:21","455"},
+				{"楚奇","232134 KB","fykhlp","2012-07-28 19:36:21","455"}
 					};
-		inDepotTable = new JTable(inDepotValue,inDepotName);
+		//假设的数据： 完善后要从数据库拿取数据来填写表格
+		
+		DefaultTableModel tableModel = new DefaultTableModel(inDepotValue,inDepotName);
+		
+		inDepotTable = new JTable(tableModel);
 		inDepotTable.setRowHeight(32);
-		inDepotTable.setAutoResizeMode(JTable.AUTO_RESIZE_NEXT_COLUMN);  
+		inDepotTable.setShowGrid(false);
+		TableColumn column = null;
+		column = inDepotTable.getColumnModel().getColumn(0);
+		column.setPreferredWidth(238);
+		column = inDepotTable.getColumnModel().getColumn(1);
+		column.setPreferredWidth(122);
+		column = inDepotTable.getColumnModel().getColumn(2);
+		column.setPreferredWidth(122);
+		column = inDepotTable.getColumnModel().getColumn(3);
+		column.setPreferredWidth(122);
+		column = inDepotTable.getColumnModel().getColumn(4);
+		column.setPreferredWidth(122);
+		
+		
+		inDepotTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		inDepotTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); 
-		inDepotTable.setIntercellSpacing(new Dimension(0, 0));   
 		
 		inDepotTable.setOpaque(false); 
 		DefaultTableCellRenderer render = new DefaultTableCellRenderer();   
 		render.setOpaque(false);
 		
-		inDepotTable.setDefaultRenderer(Object.class,render); 
-		
-		Dimension viewSize = new Dimension();
-		viewSize.width = inDepotTable.getColumnModel().getTotalColumnWidth();
-		viewSize.height = 10 * inDepotTable.getRowHeight();
-		inDepotTable.setPreferredScrollableViewportSize(viewSize);
+		inDepotTable.setDefaultRenderer(Object.class,render);
 		
 		JTableHeader header = inDepotTable.getTableHeader();
-		header.setPreferredSize(new Dimension(30, 26)); 
 		header.setOpaque(false);
 		header.getTable().setOpaque(false);
 		
