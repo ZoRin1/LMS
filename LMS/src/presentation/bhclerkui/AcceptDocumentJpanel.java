b50d151bf783e200b61cfad720011c5ecf94a80b
DoctorBear <2948363603@qq.com>
2015-12-04 20:06:03 +0800

    一点点代码

--- LMS/src/presentation/bhclerkui/AcceptDocumentJpanel.java
+++ LMS/src/presentation/bhclerkui/AcceptDocumentJpanel.java
@@ -1,8 +1,10 @@
 package presentation.bhclerkui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
@@ -10,6 +12,8 @@ import java.util.Date;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -157,7 +161,9 @@ public class AcceptDocumentJpanel extends JPanel{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				
+				panel.remove(panel2);
+				new DispatchJpanel(ui, panel, panel2);
+				panel.repaint();
 			}
 		});
 	}
@@ -167,3 +173,184 @@ public class AcceptDocumentJpanel extends JPanel{
 			g.drawImage(frameIcon.getImage(),-7,-12,null);
 	 }
 }
+class DispatchJpanel extends JPanel{
+	private ImageIcon frameIcon =new ImageIcon("picture/操作面板.png");
+	private JLabel code;
+	private JLabel code1;
+	private JLabel doName;
+	private JLabel date;
+	private JLabel date1;
+	private JLabel Tcode;
+	private JTextField tcode;
+	private JLabel member;
+	private JTextField Member;
+	private JButton returnButton;
+	private JButton yesButton;
+	private ImageIcon returnIcon=new ImageIcon("picture/返回.png");
+	private ImageIcon yesIcon=new ImageIcon("picture/确定.png");
+	public DispatchJpanel(bhclerkui ui,bhclerkJpanel panel,AcceptDocumentJpanel panel2){
+		init();
+		panel.add(this);
+		registListener(ui,panel,panel2,this);
+	}
+	private void init(){
+		Font font=new Font("幼圆",Font.BOLD,24);
+		code=new JLabel("单据编号：");
+		code.setForeground(Color.white);
+		code.setFont(font);
+		code.setBounds(30,30,125,27);
+		this.add(code);
+		
+		code1=new JLabel();
+		code1.setText(new createDocument().createDocument("派件单"));
+		code1.setForeground(Color.white);
+		code1.setFont(font);
+		code1.setBounds(155,30,131,27);
+		this.add(code1);
+		
+		doName=new JLabel("单据名：派件单");
+		doName.setForeground(Color.white);
+		doName.setFont(font);
+		doName.setBounds(360,30,175,27);
+		this.add(doName);
+		
+		date=new JLabel("到达日期：");
+		date.setForeground(Color.white);
+		date.setFont(font);
+		date.setBounds(30,97,125,27);
+		this.add(date);
+		
+		Date now = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+		String riqi = dateFormat.format( now );
+		date1=new JLabel(riqi);
+		date1.setForeground(Color.white);
+		date1.setFont(font);
+		date1.setBounds(155,97,250,27);
+		this.add(date1);
+		
+		Tcode=new JLabel("订单条形码号：");
+		Tcode.setForeground(Color.white);
+		Tcode.setFont(font);
+		Tcode.setBounds(30,164,175,27);
+		this.add(Tcode);
+		
+		tcode=new JTextField();
+		tcode.setBounds(205,164,125,27);
+		tcode.setFont(font);
+		this.add(tcode);
+		
+		member=new JLabel("派件员：");
+		member.setForeground(Color.white);
+		member.setFont(font);
+		member.setBounds(30,231,100,27);
+		this.add(member);
+		
+		Member=new JTextField();
+		Member.setBounds(130,231,125,27);
+		Member.setFont(font);
+		this.add(Member);
+		
+		returnButton=new JButton(returnIcon);
+		returnButton.setBounds(662,575,48,48);
+		returnButton.setContentAreaFilled(false);
+		this.add(returnButton);
+		
+		yesButton=new JButton(yesIcon);
+		yesButton.setBounds(602, 575,48,48);
+		yesButton.setContentAreaFilled(false);
+		this.add(yesButton);
+		
+		this.setBounds(260, 60, 730,650);
+		this.setLayout(null);
+	 	this.setOpaque(false);
+	}
+	private void registListener(final bhclerkui ui,final bhclerkJpanel panel,
+			final AcceptDocumentJpanel panel2,final DispatchJpanel dispatchJpanel) {
+		// TODO Auto-generated method stub
+		
+		returnButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				panel.remove(dispatchJpanel);
+				panel.add(panel2);
+				panel.repaint();
+			}
+		});
+		yesButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				new finishDialog(ui, "派件单创建完成", true);
+				panel.remove(dispatchJpanel);
+				panel.add(ui.operationJpanel);
+				ui.carinformationbButton.setEnabled(true);
+				ui.cashdocumentbButton.setEnabled(true);
+				ui.documentreplyButton.setEnabled(true);
+				ui.driverinformationbButton.setEnabled(true);
+				ui.loaddocumentbButton.setEnabled(true);
+				ui.acceptdocumentbButton.setEnabled(true);
+				panel.repaint();
+			}
+		});
+	}
+	public void paintComponent(Graphics g)  
+	{  
+			super.paintComponent(g);    
+			g.drawImage(frameIcon.getImage(),-7,-12,null);
+	 }
+}
+class finishDialog extends JDialog{
+	private dialogJpanel jPanel;
+	private JLabel jLabel;
+	private JButton jButton;
+	public finishDialog(JFrame frame,String title,boolean modal) {
+		super(frame,title,modal);
+		init();
+		registerListener();
+		this.setVisible(true);
+	}
+	private void init(){
+		ImageIcon yesIcon=new ImageIcon("picture/登录.png");
+		jLabel=new JLabel("派件单创建完成",jLabel.CENTER);
+		jLabel.setForeground(Color.white);
+		jLabel.setFont(new Font("幼圆",Font.BOLD,27));
+		jPanel=new dialogJpanel();
+		jButton=new JButton(yesIcon);
+		jButton.setContentAreaFilled(false);
+		jPanel.setLayout(null);
+		jButton.setBounds(218,190, 64, 64);
+		jLabel.setBounds(0, 0, 500, 200);
+		jPanel.add(jLabel);
+		jPanel.add(jButton);
+		this.add(jPanel);
+		this.setSize(500, 300);
+		Toolkit kitToolkit =Toolkit.getDefaultToolkit();
+		Dimension screenSize=kitToolkit.getScreenSize();
+		int screenWidth=screenSize.width;
+		int screenHeight=screenSize.height;
+		int dialogWidth=this.getWidth();
+		int dialogHeight=this.getHeight();
+		this.setLocation((screenWidth-dialogWidth)/2, (screenHeight-dialogHeight)/2);
+		this.setResizable(false);
+	}
+	private void registerListener(){
+		jButton.addActionListener(new ActionListener() {		
+			public void actionPerformed(ActionEvent e) {
+				finishDialog.this.dispose();
+			}
+		});
+	}
+}
+class dialogJpanel extends JPanel{
+	private ImageIcon dialogIcon=new ImageIcon("picture/背景.png");
+	public void paintComponent(Graphics g)  
+	{  
+	    super.paintComponent(g);    
+	    g.drawImage(dialogIcon.getImage(),0,0,null);
+      
+     }
+   }
